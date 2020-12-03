43f7b398004beb5f91a9044914fdddb67b7f324e
amykuner <72794385+amykuner@users.noreply.github.com>
2020-12-03 01:42:55 +0100

    Create FACApp.js
    Tab feature now functional!

---
+++ FACApp.js
@@ -0,0 +1,14 @@
+//setting the default tab as our first page
+document.getElementById("defaultTab").style.display = "block"; 
+document.getElementById("defaultTab").classList.add("active");
+
+function  chooseTab(event, tabName){ 
+var tabcontent = document.getElementsByClassName("tabcontent");
+var tablinks = document.getElementsByClassName("tablinks");
+for (var i = 0; i < tablinks.length; i++){
+  tablinks[i].classList.remove("active");
+  tabcontent[i].style.display = "none";
+} 
+  document.getElementById(tabName).style.display = "block"; 
+  document.getElementById(tabName).classList.add("active");
+}
