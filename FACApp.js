5440529f772ae79981d6e42249f0a24215241b1b
amykuner <72794385+amykuner@users.noreply.github.com>
2020-12-17 22:27:24 +0000

    Update FACApp.js
    reverting back to previous javascript because I saved over the old file by accident (great job)

--- FACApp.js
+++ FACApp.js
@@ -1,69 +1,25 @@
-<html>
-<head>
-<link rel="stylesheet" href="flexboxFACApp.css">
-<script src="https://kit.fontawesome.com/cf011bc379.js" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/css2?family=Libre+Franklin&display=swap" rel="stylesheet" type="text/css">
-</head>
-<body>
-<div class="tab">
-  <button class="tablinks" onclick="chooseTab(event, 'Other Programmers')">Other Stuff</button>
-  <button class="tablinks" onclick="chooseTab(event, 'Why FAC')" >Why FAC?</button>
-  <button class="tablinks" onclick="chooseTab(event, 'About Me')">About Me</button>
-  <button class="tablinks" onclick="chooseTab(event, 'Home')" id="defaultTab">Home</button>
-  </div>
-  
-      
-  
-  <!-- Tab content -->
-  <div id="Other Programmers" class="tabcontent">
-    <h1>Other Stuff</h1>
-    <p>Other stuff here, with links!</p>
-  </div>
-  
-  <div id="About Me" class="tabcontent">
-    <h1>About Me</h1>
-    <p>Write about me. </p>
-    <p>
-    <a href="https://www.freecodecamp.org/amykuner" target="_blank">
-      <i class="fab fa-free-code-camp" style="font-size:20vw; color: #9699f0"></i>
-    <a href="https://www.codewars.com/users/amykuner" target="_blank">
-      <i class="fab fa-jedi-order" style="font-size:20vw; color: #e005a4"></i> </a>
-     <a href="https://github.com/amykuner" target="_blank">
-      <i class="fab fa-github" style="font-size:20vw; color: #FE8171"></i></a>
-    </p>
-  </div>
-  
-   <div id="Why FAC" class="tabcontent">
-    <h1>Why FAC?</h1>
-  </div>
-  
-  
-<div id="Home" class="tabcontent">
-  <div class="hero-img">
-    <div class="hero-text">
-     <h1> Founders and Coders Application Site </h1>
-     <h2> Amy Kuner </h2>
-    </div>
-  </div>
-  <main>
-    <div class="diag-box-one">
-      <div class="dbo-content">
-        <p class="text-one-home"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
-           <!-- <img class="left-img"
-  src="https://cdn.mos.cms.futurecdn.net/zmo9JBr3EnsnDXmvYqCXqK-970-80.jpg.webp" alt="apple mac laptops with funky colours on the screens">-->
-            <p class="text-two-home"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
-      </div>
-    </div>
-    <div class="diag-box-two">
-      <div class="dbo-content-two">
-        <p class="text-three-home"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p>
-        <p class="text-four-home"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </p> 
-      </div>
-    </div>
-  </main>
-</div>  
-</body>
-  <div class="footer"> <p> This is my footer! </p> 
-</div>
-<script src="FACApp.js"></script>
-</html>
+function  chooseTab(event, tabName){ 
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++){
+      tablinks[i].classList.remove("active");
+      tabcontent[i].style.display = "none";
+    } 
+      document.getElementById(tabName).style.display = "block"; 
+      document.getElementById(tabName).classList.add("active");
+    }
+    //setting the default tab as our first page
+    document.getElementById("defaultTab").click();
+
+  function findTabWidth(number){
+  let tabButtons = document.getElementsByClassName("tablinks")
+       //console.log(tabButtons);
+    if(window.matchMedia("(max-width:700px)")){
+       for (let i=0; i < tabButtons.length; i++){
+          tabButtons[i].style.width = (screen.width / number);
+       } 
+       //document.getElementbyId
+    //console.log(tabButtons[1].style.width);
+      }
+  }
+  findTabWidth(document.getElementsByClassName("tablinks").length)
