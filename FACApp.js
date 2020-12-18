bc0685f59ba5828cf54d2257e7c413b2c4664db6
amykuner <72794385+amykuner@users.noreply.github.com>
2020-12-18 11:53:07 +0000

    Update FACApp.js
    padding added to diagonal background!

--- FACApp.js
+++ FACApp.js
@@ -31,19 +31,18 @@ function horiText(skewVal, BoxID, BoxText){
   var text = document.getElementById(BoxText);
   //skew box and then unskew text by skewval
   box.style.transform = "skewY(" + skewVal + "deg)";
-  text.style.transform = "skewY(-" +  skewVal + "deg)";
+  skewVal < 0 ? text.style.transform = "skewY(" +  Math.abs(skewVal) + "deg)": text.style.transform = "skewY(-" +  skewVal + "deg)";
+  //console.log(text.style);
   //convert skewval to radians
-  let skewRad = skewVal*Math.PI/180;
+  let skewRad = Math.abs(skewVal)*Math.PI/180;
   //set text size to 66% of screen 
   text.style.maxWidth = screen.width*0.7;
-  console.log(text.style.maxWidth);
-  console.log(Math.tan(skewRad));
-  text.style.paddingTop = Math.tan(skewRad)*(parseInt(text.style.maxWidth))/2;
-  text.style.paddingTop = Math.tan(skewRad)*(parseInt(text.style.maxWidth))/2;
-  console.log(text.style);
-  //padding top and bottom = Math.tan(skewVal)*(max width of text)/2
-  
-
+  //console.log(text.style.maxWidth);
+  //console.log(Math.tan(skewRad));
+  box.style.paddingTop = Math.tan(skewRad)*(parseInt(text.style.maxWidth))/2;
+  box.style.paddingBottom = Math.tan(skewRad)*(parseInt(text.style.maxWidth))/2;
+  //console.log(box.style);
 }
 
 horiText(11, "diag-box-one", "dbo-content");
+horiText(-4, "diag-box-two", "dbo-content-two");
